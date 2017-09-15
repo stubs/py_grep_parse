05ee087d5f2d85dbe6c8464d773dea1907c2a31a
U-GLRSLAWNY\gonzala <gonzala@win7gonzala1.GLRSLAWNY.com>
2017-09-15 17:28:58 -0400

    initial commit

--- py_grep_parse.py
+++ py_grep_parse.py
@@ -2,6 +2,7 @@
 """File assumptions: Script will assume that there is a static location where
 data wishing to be searched will live.  User will have to hard enter said path
 into line 13 of this script."""
+import pdb
 import csv
 import os
 import shlex
@@ -9,7 +10,7 @@ import sys
 from subprocess import check_call, check_output
 
 # TODO: FILL IN CUSTOM ROOT_FILE_DIR
-ROOT_FILE_DIR = os.path.join("/Path", "to", "data")
+ROOT_FILE_DIR = os.path.join("PATH", "TO", "ROOT DIR")
 EXCLUDE_DIRS = sys.argv[1]
 INCLUDES = sys.argv[2]
 PATTERN = sys.argv[3]
@@ -101,7 +102,7 @@ def write_file_headers(separators, in_file, first_row_mode=False):
 
 
 if call_grep(EXCLUDE_DIRS, INCLUDES, PATTERN, GREP_WHERE, RAW_GREP_FILE):
-    with open(RAW_GREP_FILE, "r") as infile:
+    with open(RAW_GREP_FILE, "rU") as infile:
         reader = csv.reader(infile)
 
         with open(OUT_FILE, "wb") as output:
@@ -110,30 +111,43 @@ if call_grep(EXCLUDE_DIRS, INCLUDES, PATTERN, GREP_WHERE, RAW_GREP_FILE):
             prev_row_len = 0
 
             for row_num, row in enumerate(reader):
-                path_info, line_no, first_col = row[0].split(":")
-                path_info = path_info.strip().split("/")
-                # TODO: replace "ROOT DIR" with actual root directory
-                if path_info.index("ROOT DIR"):
-                    path_info = path_info[path_info.index("ROOT DIR") + 1:]
+                try:
+                    if row[0].find(":") != -1: # check to make sure true match in grep
+                        print "PROCESSING ROW {}".format(row_num)
+                        path_info, line_no, first_col = row[0].split(":")
+                        if line_no != "1": # check to make sure there was no match on header row
+                            path_info = path_info.strip().split("/")
+                            try:
+                                if path_info.index(".") == 0:
+                                    path_info = path_info[1:]
+                            except:
+                                # TODO: replace "ROOT DIR" with actual root directory
+                                if path_info.index("ROOT DIR"):
+                                    path_info = path_info[path_info.index("ROOT DIR") + 1:]
 
-                client = path_info[0]
-                file_path = [ROOT_FILE_DIR] + path_info
-                file_path = os.path.join(*file_path)
+                            client = path_info[0]
+                            file_path = [ROOT_FILE_DIR] + path_info
+                            file_path = os.path.join(*file_path)
 
-                if prev_client == client:
-                    if prev_row_len == len(row):
-                        writer.writerow(row)
-                    elif prev_row_len != len(row):
-                        write_file_headers(SEPARATOR, file_path)
-                        writer.writerow(row)
-                else:
-                    # handle first row of the entire file
-                    if row_num == 0:
-                        write_file_headers(SEPARATOR, file_path, True)
+                            if prev_client == client:
+                                if prev_row_len == len(row):
+                                    writer.writerow(row)
+                                elif prev_row_len != len(row):
+                                    write_file_headers(SEPARATOR, file_path)
+                                    writer.writerow(row)
+                            else:
+                                # handle first row of the entire file
+                                if row_num == 0:
+                                    write_file_headers(SEPARATOR, file_path, True)
+                                else:
+                                    write_file_headers(SEPARATOR, file_path)
+                                writer.writerow(row)
+
+                            prev_client = client
+                            prev_row_len = len(row)
                     else:
-                        write_file_headers(SEPARATOR, file_path)
-                    writer.writerow(row)
-
-                prev_client = client
-                prev_row_len = len(row)
+                        #continue
+                        writer.writerow(row)
+                except Exception, e:
+                    print "ERROR WORKING WITH ROW NUMBER {}".format(row_num)
     print "\a"  # audible bell for testing
